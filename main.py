4da505dfba0faf0a6e9f1e3f71497513e915fcce
lovxonvy <87499021+triennale@users.noreply.github.com>
2022-06-13 21:42:18 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from random import randint
+with open("first_names.txt") as f1, open("last_names.txt") as f2:
+    names = list(map(lambda x: x.strip(),f1.readlines()))
+    lnames = list(map(lambda x: x.strip(),f2.readlines()))
+    for i in range(11):
+        print(f'{names[randint(0,len(names))-1]} {lnames[randint(0,len(lnames))-1]}')
